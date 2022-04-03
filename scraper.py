8274e160b3479eca71348628d3950e51ecc17fdb
DamianBartkowski <s223641@student.uek.krakow.pl>
2022-04-03 16:12:26 +0200

    03.03.2022

--- scraper.py
+++ scraper.py
@@ -7,3 +7,32 @@ response = requests.get(url)
 
 page_dom = BeautifulSoup(response.text, 'html.parser')
 
+reviews = page_dom.select('div.js_product-review')
+review = reviews.pop()
+ 
+print(type(reviews))
+print(type(page_dom))
+print(type(review))
+
+review_id =review["data-entry-id"]
+print(review_id)
+
+
+author =review.select("span.user-post__author-name").pop(0).text.strip()
+print(type(author))
+print(author)
+
+recommendation = review.select("span.user-post__author-recomendation > em").pop(0).text
+print(recommendation)
+recommendation == True if recommendation == "polecam" else False if recommendation == "Nie polecam" else None
+stars = review.select("span.user-post__score-count").pop(0).text
+stars = float(stars.split("n").pop(0).replace(",","."))
+print(type(stars))
+print(stars)
+
+concent = review.select("div.user-post__text").pop(0).string
+concent.replace("\n"," ")
+print(type(concent))
+print(concent)
+
+publish_date = review.select("span.user-post__published > time:nth-child(1)").pop("datetime")
